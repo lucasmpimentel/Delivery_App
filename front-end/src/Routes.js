017f56acf68dba98559bf951f20a2c58fa44cee2
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-08-22 20:16:33 -0300

    feat: pagina de rotas

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Login from './pages/login';
+import SignUp from './pages/sign-up';
 
 export default function Index() {
-
+  return (
+    <Routes>
+      <Route path="/login" element={ <Login /> } />
+      <Route path="cadastro" element={ <SignUp /> } />
+    </Routes>
+  );
 }
