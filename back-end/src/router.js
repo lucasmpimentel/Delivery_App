e7d07b41a274f516502b2e1d6b9ac5303b1c51e5
marcos-rafael-br <mrpf10@ehotmail.com>
2022-08-23 19:52:02 -0300

    feat: validação de e-mail e password usando joi

--- back-end/src/router.js
+++ back-end/src/router.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const loginController = require('./controllers/controller.login');
+const { validateLogin } = require('./middleware/middleware.login');
 
 const router = express.Router();
 
-router.use('/login', loginController);
+router.use('/login', validateLogin, loginController);
 
 module.exports = router;
-
