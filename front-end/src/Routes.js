5a2958b0961e88aaa0ee1fb17bff3166c8a99aa8
Marina Barros <marinabarrossilva@gmail.com>
2022-08-23 17:21:01 -0300

    style: pasta sign-up renomeada (req 6)

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/login';
-import SignUp from './pages/sign-up';
+import Register from './pages/register';
 
 export default function Index() {
   return (
     <Routes>
       <Route path="/login" element={ <Login /> } />
-      <Route path="cadastro" element={ <SignUp /> } />
+      <Route path="/register" element={ <Register /> } />
     </Routes>
   );
 }
