0f206291cd1397fed0601b9264daeff0e9e51aff
Marina Barros <marinabarrossilva@gmail.com>
2022-08-23 17:07:24 -0300

    feat: criada a tela de login (req 2 e 3)

--- front-end/src/pages/login/index.jsx
+++ front-end/src/pages/login/index.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-nested-ternary */
 import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 import Context from '../../context/context';
 // import makeLogin from '../../global/services/login.service';
 // import storage from '../../global/services/storage.services';
@@ -28,6 +29,17 @@ export default function Login() {
     try {
       if (user.email && user.password) {
         const checkEmail = regEx.test(user.email);
+        if (!checkEmail) {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            // text: 'Something went wrong!',
+            footer: `
+            <span data-testid="common_login__element-invalid-email">
+              Invalid email
+            </span>`,
+          });
+        }
         const checkPassword = user.password.length >= MIN_PASS;
         if (checkEmail && checkPassword) {
           setIsLoading(true);
@@ -44,8 +56,8 @@ export default function Login() {
     } catch (err) {
       setIsLoading(false);
       setAuthorized(false);
-      setErrorMsg(err.message);
-      return setModalOpen(true);
+      // setErrorMsg(err.message);
+      // return setModalOpen(true);
     }
   };
 
@@ -64,6 +76,7 @@ export default function Login() {
         <input
           aria-label="email"
           id="email"
+          data-testid="common_login__input-email"
           type="email"
           name="email"
           placeholder="Email"
@@ -73,16 +86,27 @@ export default function Login() {
         <input
           aria-label="password"
           id="password"
+          data-testid="common_login__input-password"
           type="password"
           name="password"
           placeholder="Senha"
           value={ user.password }
           onChange={ handleChange }
         />
-        <button type="submit">Entrar</button>
-        <button type="button" onClick={ () => navigate('/cadastro') }>
+        <button data-testid="common_login__button-login" type="submit">Entrar</button>
+        <button
+          data-testid="common_login__button-register"
+          type="button"
+          onClick={ () => navigate('/cadastro') }
+        >
           Cadastre-se!
         </button>
+        {/* <span
+          data-testid="common_login__element-invalid-email"
+          className="hidden"
+        >
+          Login inválido!
+        </span> */}
       </form>
     </main>
   );
