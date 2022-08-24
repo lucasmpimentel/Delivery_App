f3ac55abe804c25b74496ccb9c868a6db3e06d50
marcos-rafael-br <mrpf10@ehotmail.com>
2022-08-23 22:16:51 -0300

    refactor: nomenclaturas model, migration - sequelize

--- back-end/src/database/models/user.js
+++ back-end/src/database/models/user.js
@@ -1,6 +1,6 @@
 const User = (sequelize, DataTypes) => {
   const User = sequelize.define(
-    'Users',
+    'user',
     {
       id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
       name: DataTypes.STRING,
@@ -10,8 +10,6 @@ const User = (sequelize, DataTypes) => {
     },
     {
       timestamps: false,
-      freezeTableName: true,
-      tableName: 'users'
     }
   );
   return User;
