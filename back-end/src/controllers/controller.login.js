b580fd7906b432565c90e714f046712dc15914dd
Felipe5215 <felipe.eleotero@gmail.com>
2022-08-25 10:03:59 -0300

    feat: registrando user

--- back-end/src/controllers/controller.login.js
+++ back-end/src/controllers/controller.login.js
@@ -1,14 +1,13 @@
-const express = require('express');
-
 const loginService = require('../services/login.service');
 
-const loginRouter = express.Router();
+const loginUser = async (req, res) => {
+  const token = await loginService.authenticate(req.body);
+  res.status(200).json({ token });
+};
 
-loginRouter.post(
-  '/',
-  async (req, res) => {
-    const token = await loginService.authenticate(req.body);
-    res.status(200).json({ token });
-  },
-);
-module.exports = loginRouter;
+const registerUser = async (req, res) => {
+  const token = await loginService.createUser(req.body);
+  res.status(200).json({ token });
+};
+
+module.exports = { loginUser, registerUser };
--- back-end/src/services/login.service.js
+++ back-end/src/services/login.service.js
@@ -19,4 +19,18 @@ const authenticate = async ({ email, password }) => {
   const token = generateJWTToken(userFound);
   return token;
 };
-module.exports = { authenticate };
+
+const createUser = async ({ name, password, email, role }) => {
+  const encrypted = md5(password);
+
+  const createdUser = await user.create({
+    email,
+    password: encrypted,
+    name,
+    role,
+  });
+
+  return generateJWTToken(createdUser);
+};
+
+module.exports = { authenticate, createUser };
