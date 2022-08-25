a44cbee92e8041c484bdf4c58bc094ac4f36a46b
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-08-25 20:15:44 -0300

    refactor: arrumando a rota de checkout

--- back-end/src/controllers/controller.checkout.js
+++ back-end/src/controllers/controller.checkout.js
@@ -1,15 +1,9 @@
-const express = require('express');
-
 const checkoutService = require('../services/checkout.service');
 
-const checkoutRouter = express.Router();
-
-checkoutRouter.post(
-  '/',
-  async (req, res) => {
+const checkout = async (req, res) => {
     const { id } = res.locals;
     const response = await checkoutService.create(req.body, id);
     res.status(200).json(response);
-  },
-);
-module.exports = checkoutRouter;
+  };
+
+module.exports = checkout;
