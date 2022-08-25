d7d9299de37c26fe14fcca8fcf882d73e2aa3200
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-08-25 15:42:28 -0300

    feat: rota de checkou em andamento, rever transaction

--- back-end/src/router.js
+++ back-end/src/router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const loginController = require('./controllers/controller.login');
 const productsController = require('./controllers/controller.products');
+const checkoutController = require('./controllers/controller.checkout');
 const authenticateMiddleware = require('./middleware/middleware.auth');
 const { validateLogin } = require('./middleware/middleware.login');
 
@@ -10,6 +11,6 @@ router.use('/login', validateLogin, loginController);
 
 router.use('/products', authenticateMiddleware, productsController);
 
-router.use('/checkout', checkoutController)
+router.use('/checkout', checkoutController);
 
 module.exports = router;
--- back-end/src/services/checkout.service.js
+++ back-end/src/services/checkout.service.js
@@ -1,8 +1,22 @@
-const { Sales } = require('../database/models');
+const { Sales, salesProduct } = require('../database/models');
+const { sequelize } = require('../database/models')
 
-const create = async () => {
-  const allProducts = await Sales.create();
-  return allProducts;
+const create = async ({
+  user_id,seller_id, total_price, delivery_address, delivery_number,
+  sale_date, status: saleStatus, product_id, quantity,}) => {
+  try { await sequelize.transaction(async function (transaction) {
+    const allSales = await Sales.create({
+      user_id,seller_id, total_price, delivery_address, delivery_number,
+      sale_date, status: saleStatus },
+    { transaction });
+     await salesProduct.create({
+      sale_id: allSales.id, product_id, quantity,
+    }, { transaction });
+    return allSales; });
+  } catch (error) {
+    console.log('error');
+  } 
 };
 
 module.exports = { create };
+ 
