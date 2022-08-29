b9bba5a7de96255282cb28d64d881d038ad98d64
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-08-29 16:21:11 -0300

    fix: login and register functions

--- front-end/src/hooks/Authorizer.jsx
+++ front-end/src/hooks/Authorizer.jsx
@@ -1,22 +1,31 @@
 import React, { useContext } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import jwt from 'jwt-decode';
 import context from '../context/context';
 import auth from '../utils/auth';
 
 function Authorizer() {
   const {
     authorized,
+    setSessionUser,
   } = useContext(context);
 
   const verifyAuth = () => {
-    const data = auth.checkAuth();
-    if (data) return data;
+    const token = auth.checkAuth();
+    if (token) {
+      const { data } = jwt(token);
+      const { id, name, email, role } = data;
+      if (id && name && email && role) {
+        return data;
+      }
+    }
     return false;
   };
 
   if (!authorized) {
     try {
       const user = verifyAuth();
+      setSessionUser(user);
       if (!user) {
         return <Navigate to="/login" />;
       }
