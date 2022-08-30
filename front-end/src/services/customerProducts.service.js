93d23f82a1871c00c223802aadf64d34fa24023e
Marina Barros <marinabarrossilva@gmail.com>
2022-08-30 00:15:10 -0300

    feat: criada a função de get all products entre back e front

---
+++ front-end/src/services/customerProducts.service.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import Swal from 'sweetalert2';
+
+const URL = process.env.REACT_APP_HOSTNAME;
+const PORT = process.env.REACT_APP_BACKEND_PORT;
+
+const host = axios.create({
+  baseURL: `http://${URL}:${PORT}`,
+  timeout: 10000,
+});
+
+export default async function getAllProducts() {
+  try {
+    const { data } = await host.get('/products');
+    return data;
+  } catch (err) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: `${err.message}`,
+      footer: `
+      <span>
+        Error!
+      </span>`,
+    });
+  }
+}
