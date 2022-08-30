9ec4abe1a5ab862fcbb8f9f3ee65e09455b6dd86
CRafaelS <carlos.rafael@engenharia.ufjf.br>
2022-08-30 16:05:02 -0300

    feat: validando o token na rota sales

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -3,12 +3,13 @@ const checkoutRouter = require('./checkoutRouter');
 const loginRouter = require('./loginRouter');
 const productsRouter = require('./productsRouter');
 const salesRouter = require('./salesRouter');
+const authenticateMiddleware = require('../middleware/middleware.auth');
 
 const routes = express.Router();
 
 routes.use(loginRouter);
 routes.use(productsRouter);
 routes.use(checkoutRouter);
-routes.use('/sales', salesRouter);
+routes.use('/sales', authenticateMiddleware, salesRouter);
 
 module.exports = routes;
