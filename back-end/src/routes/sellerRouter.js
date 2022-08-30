e792f282974dccd614d6207f9f17b3b992ecaacf
CRafaelS <carlos.rafael@engenharia.ufjf.br>
2022-08-30 19:22:14 -0300

    feat: validação da entrada dos status

--- back-end/src/routes/sellerRouter.js
+++ back-end/src/routes/sellerRouter.js
@@ -1,9 +1,9 @@
 const sellerRouter = require('express').Router();
-
+const { validateStatus } = require('../middleware/middleware.status');
 const {
     updateStatus,
 } = require('../controllers/controller.seller');
 
-sellerRouter.patch('/seller/:id', updateStatus);
+sellerRouter.patch('/seller/:id', validateStatus, updateStatus);
 
 module.exports = sellerRouter;
