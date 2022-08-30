1dc40d49a5aca7ead668bd1744585634d959df04
Marina Barros <marinabarrossilva@gmail.com>
2022-08-29 21:56:56 -0300

    feat: importação do componente ProductsTable

--- front-end/src/pages/home/index.jsx
+++ front-end/src/pages/home/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import ProductsTable from '../../components/ProductsTable';
 
 export default function Home() {
   return (
     <main>
       <div><h1>HEADER</h1></div>
       <section>
-        CARDS
+        <ProductsTable />
       </section>
     </main>
   );
