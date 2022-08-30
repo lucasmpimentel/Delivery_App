8f29246d62b3e61e975efb24f9480c716427dfae
Marina Barros <marinabarrossilva@gmail.com>
2022-08-30 15:39:34 -0300

    fix: recolocação do middleware de autenticação

--- back-end/src/routes/productsRouter.js
+++ back-end/src/routes/productsRouter.js
@@ -2,5 +2,5 @@ const productsRouter = require('express').Router();
 const { getAllProducts } = require('../controllers/controller.products');
 const authenticateMiddleware = require('../middleware/middleware.auth');
 
-productsRouter.get('/products', getAllProducts);
+productsRouter.get('/products', authenticateMiddleware, getAllProducts);
 module.exports = productsRouter;
