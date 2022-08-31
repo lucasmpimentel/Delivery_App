030a0da3807c8a118df294dd7e1d41d22adc4636
CRafaelS <carlos.rafael@engenharia.ufjf.br>
2022-08-31 19:06:39 -0300

    fix: erro de lint

--- back-end/src/routes/sellerRouter.js
+++ back-end/src/routes/sellerRouter.js
@@ -2,7 +2,7 @@ const sellerRouter = require('express').Router();
 const { validateStatus } = require('../middleware/middleware.status');
 const {
     updateStatus,
-    getSeller
+    getSeller,
 } = require('../controllers/controller.seller');
 
 sellerRouter.patch('/seller/:id', validateStatus, updateStatus);
