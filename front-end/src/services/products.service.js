b75b083125c6a162944ff2fea0107a5832273fe2
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-08-31 05:16:16 -0300

    refactor: padronizado arquivo de conexão de produtos feito pela @BSMarina

---
+++ front-end/src/services/products.service.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import storage from '../utils/storage';
+
+const URL = process.env.REACT_APP_HOSTNAME;
+const PORT = process.env.REACT_APP_BACKEND_PORT;
+
+const token = storage.getLocalStorage('token');
+
+const host = axios.create({
+  baseURL: `http://${URL}:${PORT}`,
+  headers: { authorization: token },
+  timeout: 10000,
+});
+
+export default async function getAllProducts() {
+  try {
+    const data = await host.get('/products').then((res) => res.data);
+    return data;
+  } catch (err) {
+    Swal.fire({
+      icon: 'warning',
+      title: 'Oops...',
+      text: 'Unauthorized!',
+    });
+    return false;
+  }
+}
