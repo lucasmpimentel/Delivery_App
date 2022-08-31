5ad2af27d83ef2326fc3418e965586127640368c
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-08-31 05:42:20 -0300

    refactor: trocar nome da pagina home para products

--- front-end/src/pages/products/index.jsx
+++ front-end/src/pages/products/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '../../components/navbar';
 import ProductsTable from '../../components/ProductsTable';
 
-export default function Home() {
+export default function Products() {
   return (
     <main>
       <Navbar />
