264f9be54d8288a3795b27a8af1600d2656f863a
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-09-03 01:38:23 -0300

    style: adicionando estilos na barra de navegação

--- front-end/src/components/Navbar/index.jsx
+++ front-end/src/components/Navbar/index.jsx
@@ -1,9 +1,12 @@
 import React, { useContext, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
 import Context from '../../context/context';
 import storage from '../../utils/storage';
+import * as My from './style';
 
 export default function Navbar() {
+  const navigate = useNavigate();
   const { sessionUser, setSessionUser } = useContext(Context);
 
   useEffect(() => {
@@ -12,35 +15,43 @@ export default function Navbar() {
   }, []);
 
   return (
-    <div className="nav">
-      <Link
-        data-testid="customer_products__element-navbar-link-products"
-        to="/customer/products"
-      >
-        PRODUTOS
+    <My.NavBar className="nav">
+      <My.ToolBar>
+        <My.Div>
+          <My.NavButton onClick={ () => navigate('/customer/products') }>
+            <Typography
+              variant="h6"
+              data-testid="customer_products__element-navbar-link-products"
+            >
+              PRODUTOS
+            </Typography>
+          </My.NavButton>
+          <My.NavButton onClick={ () => navigate('/customer/orders') }>
+            <Typography
+              variant="h6"
+              data-testid="customer_products__element-navbar-link-products"
+            >
+              MEUS PEDIDOS
+            </Typography>
+          </My.NavButton>
+        </My.Div>
+        <My.Div>
+          <NavLink
+            data-testid="customer_products__element-navbar-user-full-name"
+            to="/"
+          >
+            {sessionUser.name}
 
-      </Link>
-      <Link
-        data-testid="customer_products__element-navbar-link-orders"
-        to="/customer/checkout"
-      >
-        MEUS PEDIDOS
+          </NavLink>
+          <NavLink
+            data-testid="customer_products__element-navbar-link-logout"
+            to="/logout"
+          >
+            Sair
 
-      </Link>
-      <Link
-        data-testid="customer_products__element-navbar-user-full-name"
-        to="/"
-      >
-        {sessionUser.name}
-
-      </Link>
-      <Link
-        data-testid="customer_products__element-navbar-link-logout"
-        to="/logout"
-      >
-        Sair
-
-      </Link>
-    </div>
+          </NavLink>
+        </My.Div>
+      </My.ToolBar>
+    </My.NavBar>
   );
 }
