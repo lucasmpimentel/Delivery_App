26a1d7b623e5bee0a032c8d9e509093a54fc8ae1
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-09-05 17:17:35 -0300

    feat: configurações do seller e autenticação

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Authorizer from './hooks/Authorizer';
+import AuthSeller from './hooks/AuthSeller';
 import Login from './pages/login';
 import Register from './pages/register';
 import Products from './pages/products';
@@ -16,6 +17,11 @@ export default function Index() {
       <Route path="login" element={ <Login /> } />
       <Route path="register" element={ <Register /> } />
       <Route path="logout" element={ <Logoff /> } />
+      <Route path="seller" element={ <AuthSeller /> }>
+        <Route index element={ <Navigate to="orders" /> } />
+        <Route path="orders" element={ <Orders /> } />
+        <Route path="orders/:id" element={ <OrdersDetails /> } />
+      </Route>
       <Route path="customer" element={ <Authorizer /> }>
         <Route index element={ <Navigate to="products" /> } />
         <Route path="products" element={ <Products /> } />
