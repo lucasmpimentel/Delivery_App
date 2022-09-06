ced9ba5670b1efcb7f824a051280ad0a369acede
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-09-06 19:46:08 -0300

    style: login page

--- front-end/src/assets/styles/themes/default.js
+++ front-end/src/assets/styles/themes/default.js
@@ -1,3 +1,20 @@
-export default {
-  colors: {},
-};
+import { createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#ffbf9b',
+      main: '#ff9f68',
+      dark: '#ff7f35',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
+
+export default theme;
